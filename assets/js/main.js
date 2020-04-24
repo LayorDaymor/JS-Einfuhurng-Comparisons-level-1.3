f91766a0c359eba91b53428f787bf29f0fa6f51b
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-24 18:33:22 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+let i = 1;
+let lastName = "Johnson";
+let x = {
+firstName: "John",
+lastName: "Doe"
+};
+let j ="2";
+let status = true;
+var hello = hello;
+let helloWorld = "hello";
+
+console.log(typeof (i))
+console.log(typeof (lastName))
+console.log(typeof (x))
+console.log(typeof (status))
+console.log(typeof (hello))
+console.log(typeof (helloWorld))
+
